5fa286ceb9af539c5ca57af4b8ecaccacf021dc8
Mikhail <32320035+R-Mikhail@users.noreply.github.com>
2017-10-29 15:02:16 +0300

    Update

--- Client.py
+++ Client.py
@@ -1,20 +1,19 @@
 import socket
 
-class Connection:
-    adress = '127.0.0.1'
+class Client:
+    address = '127.0.0.1'
     port = 1234
     coding = 'utf-8'
 
-class Client:
-    #Сетевая часть клиента.
     def __init__(self):
+        #Установление соединения
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect((Connection.adress, Connection.port))
+        self.s.connect((address, port))
 
     def send(self, msg):
         #Отправка сообщения на сервер
-        self.s.send(msg.encode(Connection.coding))
+        self.s.send(msg.encode(coding))
 
     def recv(self, size = 1024):
         #Получение сообщения от сервера
-        return self.s.recv(size).decode(Connection.coding)
+        return self.s.recv(size).decode(coding)
