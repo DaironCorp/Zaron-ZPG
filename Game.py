70864d8a4db0b73ccf0000f0a6c5c3e30188fae7
Saturon150300 <30622777+Saturon150300@users.noreply.github.com>
2017-10-29 15:32:56 +0300

    CL

--- Game.py
+++ Game.py
@@ -7,7 +7,7 @@ Class World:
 	def __init__(self):
 		CL = ["1;1","1;2","2;1","2;2","2;3"]
 
-	def askter(x,z):
-		return(self.WorldCL.terry(x,z))
+	def askter(x):
+		return(self.WorldCL.terry(x))
 
 world = World()
