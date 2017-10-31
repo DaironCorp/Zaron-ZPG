430e0217b57d1fc4edb7ff4a705e98bde5f011b4
Saturon150300 <30622777+Saturon150300@users.noreply.github.com>
2017-10-31 15:17:49 +0300

    Базовые харатеристики клетки

---
+++ Moduls.py
@@ -0,0 +1,21 @@
+class Biom:
+	class SRD:
+		N = 1
+		li = 'комфортно'
+	class TEP:
+		N = 2
+		li = 'Жарко'
+	class HOL:
+		N = 3
+		li = 'холодно'
+	class VDA:
+		N = 4
+		li = 'море! тону'
+
+class LND:
+	class GOR:
+		li = 'высоко'
+	class RVN:
+		li = 'средне'
+	class NIZ:
+		li = 'низина'
