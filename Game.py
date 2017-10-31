8ffcb91919508a738301464c694cdd6b6fbf4ac9
Saturon150300 <30622777+Saturon150300@users.noreply.github.com>
2017-10-31 16:20:44 +0300

    asd

--- Game.py
+++ Game.py
@@ -1,13 +1,24 @@
 import WorldCL
 import network
 import socket
-import os
 
-Class World:
+class World:
 	def __init__(self):
-		CL = ["1;1","1;2","2;1","2;2","2;3"]
+		a = []
+		with open('Cletki.txt',r) as CLETKI:
+			for line in CLETKI:
+				a.append(
+					lambda line: 
+					x=line.split()[0] 
+					y=line.split()[1] 
+					z=line.split()[2:]
+					return([x,y,z])
+				 	)
+		for b in a: 
+			print(b)
+		g=input()
 
-	def askter(x):
-		return(self.WorldCL.terry(x))
+	def askter(x,y):
+		return(self.WorldCL.terry(x,y))
 
 world = World()
--- WorldCL.py
+++ WorldCL.py
@@ -1,14 +1,5 @@
-Class CL:
-	A["1;1"]:
-		text = 'Base cl 11'
-	A["1;2"]:
-		text = 'Base cl 12'
-	A["2;1"]:
-		text = 'Base cl 21'
-	A["2;2"]:
-		text = 'Base cl 22'
-	A["2;3"]:
-		text = 'Base cl 23'
+import Moduls
 
-def terry(x):
-	return(CL.A[x].text)
+Class CL(self,x,y):
+	def terry():
+	return()
